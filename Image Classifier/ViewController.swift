568c7475ed7932f199e8a2c5288bce99d6afa940
LafuenteR <lafuente1101403@ceu.edu.ph>
2019-12-01 16:16:59 +0800

    Initial Commit

---
+++ Image Classifier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Image Classifier
+//
+//  Created by Ruel Lafuente on 01/12/2019.
+//  Copyright © 2019 LafuenteR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

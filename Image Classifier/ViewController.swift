15d259ffbfa5e554d590a833c9fd570dce900497
LafuenteR <lafuente1101403@ceu.edu.ph>
2019-12-01 23:12:36 +0800

    image classifier codes

--- Image Classifier/ViewController.swift
+++ Image Classifier/ViewController.swift
@@ -7,12 +7,64 @@
 //
 
 import UIKit
+import AVKit
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
+    @IBOutlet weak var imageClassifierLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        
+         guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
+        
+        let capturePreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(capturePreviewLayer)
+        capturePreviewLayer.frame = view.frame
+        capturePreviewLayer.bounds.size.height = self.view.bounds.size.height - 200
+        print(view.frame, "hehe")
+        
+        let captureDataOutput = AVCaptureVideoDataOutput()
+        captureDataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(captureDataOutput)
+        
+    }
+    
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        print("hey")
+        
+        guard let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        guard let squeezeNetmodel = try? VNCoreMLModel(for: SqueezeNet().model) else { return }
+        
+        let coreMLRequest = VNCoreMLRequest(model: squeezeNetmodel) { (finishedRequest, Error) in
+            
+            print("hoooy")
+            guard let results = finishedRequest.results as? [VNClassificationObservation] else { return }
+            
+            guard let firstObservation = results.first else { return }
+            
+            print(firstObservation.identifier, firstObservation.confidence)
+            
+            DispatchQueue.main.async {
+                self.imageClassifierLabel.text = String(firstObservation.identifier) + " - " + String(firstObservation.confidence)
+            }
+            
+        }
+        
+        try? VNImageRequestHandler(cvPixelBuffer: imageBuffer, options: [:]).perform([coreMLRequest])
     }
 
 
